0f5c5c1b2589b7a4f575e4cf124c060444374f05
freddyeleazar <freddyeleazar@gmail.com>
2021-03-27 16:09:28 -0300

    no message

--- js/functions.js
+++
@@ -1,8 +0,0 @@
-function resizeIframeToFitContent(iframe){
-	if(iframe.contentWindow.document.title.includes("Unity WebGL Player")){
-	    iframe.style.width  = iframe.contentWindow.document.getElementById("unity-canvas").style.width;
-	    iframe.style.height = iframe.contentWindow.document.getElementById("unity-canvas").style.height;
-		iframe.contentWindow.focus();
-	}
-	iframe.style.border = "none";
-}
